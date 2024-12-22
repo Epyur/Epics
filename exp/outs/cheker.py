8153e16119da749f23bc4e19cbec70e9a642a098
Epyur <eugpolishchuk@gmail.com>
2024-12-22 20:44:00 +0300

    sv1

---
+++ exp/outs/cheker.py
@@ -0,0 +1,16 @@
+from exp.outs.inc_sources.aim_book import *
+from exp.outs.inc_sources.inc_file import inc_sheet
+
+# формируем список данных входящей заявки, отсутствующих в архиве
+def inc_data():
+    inc_row = []
+    for ii in range(inc_sheet.nrows):  # в цикле по количеству всех строк
+        data = inc_sheet.cell_value(ii, 0)  # получаем значение ячейки (ii-строка, 0-столбец)
+        if str(new_id()) == str(data):  # сравниваем заданное значение с полученным, если истина
+            for i in range(inc_sheet.ncols):  # в цикле по количеству столбцов
+                a = inc_sheet.cell_value(ii, i)
+                inc_row.append(a)
+    return inc_row
+
+print(inc_title_row)
+print(inc_data())
