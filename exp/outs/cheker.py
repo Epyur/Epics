787110285f899d1f4f4ad0379d307defee790708
Epyur <eugpolishchuk@gmail.com>
2024-12-24 08:26:53 +0300

    sv

--- exp/outs/cheker.py
+++ exp/outs/cheker.py
@@ -4,11 +4,11 @@ from inc_files import *
 # формируем список данных входящей заявки, отсутствующих в архиве
 def inc_data():
     inc_row = []
-    for ii in range(inc_sheet.nrows):  # в цикле по количеству всех строк
-        data = inc_sheet.cell_value(ii, 0)  # получаем значение ячейки (ii-строка, 0-столбец)
+    for ii in range(rb_inc.nrows):  # в цикле по количеству всех строк
+        data = rb_inc.cell_value(ii, 0)  # получаем значение ячейки (ii-строка, 0-столбец)
         if str(new_id()) == str(data):  # сравниваем заданное значение с полученным, если истина
-            for i in range(inc_sheet.ncols):  # в цикле по количеству столбцов
-                a = inc_sheet.cell_value(ii, i)
+            for i in range(rb_inc.ncols):  # в цикле по количеству столбцов
+                a = rb_inc.cell_value(ii, i)
                 inc_row.append(a)
     return inc_row
 
--- exp/outs/inc_files.py
+++ exp/outs/inc_files.py
@@ -1,34 +1,49 @@
 import xlrd
 
-# разбираем файл заявок
-rb = xlrd.open_workbook('../excel/inc.xls', formatting_info=True)
-inc_sheet = rb.sheet_by_index(0)
-inc_index_column = inc_sheet.col_values(colx=0)
-inc_title_row = inc_sheet.row_values(rowx=0)
+# функции разборки книги
+def open_file(file, ind): # вызов файла xls и 1 листа в ней
+    rb = xlrd.open_workbook(file, formatting_info=True)
+    inc_sheet = rb.sheet_by_index(ind)
+    return inc_sheet
+rb_inc = open_file('../excel/inc.xls', 0)
+
+def chose_column(col): # вызов файла xls
+    index_column = rb_inc.col_values(colx=col)
+    return index_column
+
+def chose_row(row): # вызов файла xls
+    title_row = rb_inc.row_values(rowx=row)
+    return title_row
+
+
+# разбираем лист заявок
+rb_inc_column = chose_column(0)
+rb_inc_row = chose_row(0)
+
 # считаем элементы в титульной строке
 def inc_title_inf():
     col_index = []
     count_ = -1
-    for i in inc_title_row:
+    for i in rb_inc_row:
         count_ += 1
         col_index.append(count_)
-    inf_dict_col = dict(zip(col_index, inc_title_row)) # проверочный словарь, для сверки индексов и наименований столбцов
+    inf_dict_col = dict(zip(col_index, rb_inc_row)) # проверочный словарь, для сверки индексов и наименований столбцов
     return inf_dict_col
 
 # считываем колонку идентификаторов
 def inc_row_inf():
     row_index = []
     count_row = -1
-    for i in inc_index_column:
+    for i in rb_inc_column:
         count_row += 1
         row_index.append(count_row)
-    inf_dict_row = dict(zip(row_index, inc_index_column)) # проверочный словарь, для сверки индексов и значений строк
+    inf_dict_row = dict(zip(row_index, rb_inc_column)) # проверочный словарь, для сверки индексов и значений строк
     return inf_dict_row
 
 # Формируем список входящих идентификаторов
 def inc_id_list():
     inc_id_list = []
-    inc_id = inc_index_column[1:]
+    inc_id = rb_inc_column[1:]
     for i in inc_id:
         inc_id_list.append(i)  # список входящих идентификаторов
     return inc_id_list
