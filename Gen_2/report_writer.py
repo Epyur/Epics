7166c7afc67fc76f764f2921f4d389dca14e523f
Epyur <eugpolishchuk@gmail.com>
2024-12-30 22:46:12 +0300

    30

--- Gen_2/report_writer.py
+++ Gen_2/report_writer.py
@@ -2,4 +2,6 @@ import pandas as pd
 from Gen_2.workers.methods import *
 from Gen_2.workers.xlsx import *
 
-print(Combustion.title_row)
+print(c_title_inf_dict)
+print(c_id_list())
+print(c_ser_number)
--- Gen_2/workers/methods.py
+++ Gen_2/workers/methods.py
@@ -1,6 +1,29 @@
 from Gen_2.workers.xls import *
 import math
+# Разбираем результаты испытаний на горючесть
+# разбираем книгу результатов
+c_book = open_file(comb_book, 0) # open the book of measures
+c_title_row = c_book.row_values(rowx=0) # form title row names
+c_id_column = c_book.col_values(colx=2) # form list of incoming request id
+c_ser_number = c_book.col_values(colx=24) # form list of research number
 
-class Combustion:
-    book = open_file(comb_book, 0) # open the book of measures
-    title_row = book.row_values(rowx=0) # form title row names
+
+    # формируем библиотеку титулов
+def title_dict(title):
+    col_index = []
+    count_ = -1
+    for i in c_title_row:
+        count_ += 1
+        col_index.append(count_)
+    inf_dict_col = dict(zip(col_index, title))  # проверочный словарь, для сверки индексов и наименований столбцов
+    return inf_dict_col
+
+c_title_inf_dict = title_dict(c_title_row)
+
+# Формируем список входящих идентификаторов
+def c_id_list():
+    c_id_list = []
+    c_id = c_id_column[0:]
+    for i in c_id:
+        c_id_list.append(i)  # список входящих идентификаторов
+    return c_id_list
